68584f7a7037763deaff1febfa3ab2858396782b
Sam Vain <sam.vain@hotmail.co.uk>
2022-02-01 10:43:43 +0000

    Renamed Inputs to Login

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import ReactDOM from 'react-dom';
-import Inputs from './Inputs.js';
+import Login from './Login.js';
+
+/*
+This is the entry point for the application
+*/
 
 class App extends Component {
   render(){
 
     return (
-      <Inputs />
+      <Login />
     );
   }
 }
