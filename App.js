65073bf73d26a19be710e26d40468f03f92df54f
Sam Vain <sam.vain@hotmail.co.uk>
2022-02-04 16:37:19 +0000

    Moved Navigation Code to its own file

--- App.js
+++ App.js
@@ -9,32 +9,30 @@ import SpacebookHeader from './js/components/SpacebookHeader';
 import HomePage from './js/pages/HomePage';
 import AccountPage from './js/pages/AccountPage';
 import SearchPage from './js/pages/SearchPage';
-//import HomeStack from './routes/homeStack'
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createAppContainer } from "react-navigation";
+import HomeStack from './routes/homeStack'
 import { NavigationContainer } from '@react-navigation/native';
 
 'use strict'
-
+/* 
 const Stack = createNativeStackNavigator();
 
 function HomeStack() {
     return(
         <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomePage} />
             <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="Signup" component={SignupPage} />
-            <Stack.Screen name="Home" component={HomePage} />
             <Stack.Screen name="Search" component={SearchPage} />
             <Stack.Screen name="Account" component={AccountPage} />
         </Stack.Navigator>
     );
 }
-
+ */
 /*
 This is the entry point for the application
 */
 
-export default function App({}) {
+export default function App() {
   //const navigationRef = useRef(null)
 
   return(
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,4 +1,5 @@
-/* import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useRef } from 'react';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createAppContainer } from "react-navigation";
 import HomePage from "../js/pages/HomePage";
 import SearchPage from "../js/pages/SearchPage";
@@ -11,14 +12,13 @@ const Stack = createNativeStackNavigator();
 function HomeStack() {
     return(
         <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomePage} />
             <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="Signup" component={SignupPage} />
-            <Stack.Screen name="Home" component={HomePage} />
             <Stack.Screen name="Search" component={SearchPage} />
             <Stack.Screen name="Account" component={AccountPage} />
         </Stack.Navigator>
     );
 }
 
-//const HomeStack = createStackNavigator(screens);
-export default HomeStack; */
+export default HomeStack;
